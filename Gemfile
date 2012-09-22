d5017471c15ce55507909437ecd79d925200d3ec
James Ma <jamesma.1987@gmail.com>
2012-09-21 22:28:34 -0400

    Finish user signup.

--- Gemfile
+++ Gemfile
@@ -29,6 +29,10 @@ group :test do
   gem 'capybara', '1.1.2'
   gem 'rb-fsevent', '0.9.1', :require => false
   gem 'growl', '1.0.3'
+
+  # factories to serve as a convenient way to define objects and insert
+  # them into the database
+  gem 'factory_girl_rails', '1.4.0'
 end 
 
 group :production do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # flash message
+      flash[:success] = "Welcome to the Sample App!"
+      # handle a successful save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
